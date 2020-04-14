eac809e9051821db8c270bd1f71dbd81ca57cf9e
Gerardo Aldana <47541281+GeoDude1@users.noreply.github.com>
2020-04-14 13:13:05 -0700

    Update servernew.c

--- servernew.c
+++ servernew.c
@@ -88,5 +88,14 @@ int main(int argc, char *argv[])
       printf("The new file created is glad5.txt");
      close(newsockfd);
      close(sockfd);
+     printf("Server_IP_Address: %s\n", json_object_get_string(Server_IP_Address));
+  printf("Source_Port_Number_UDP: %s\n", json_object_get_string(Source_Port_Number_UDP));
+  printf("Destination_Port_Number_TCP_Head: %s\n", json_object_get_string(Destination_Port_Number_TCP_Head));
+  printf("Destination_Port_Number_TCP_Tail: %s\n", json_object_get_string(Destination_Port_Number_TCP_Tail));
+  printf("Port_Number_TCP: %s\n", json_object_get_string(Port_Number_TCP));
+  printf("Size_UDP_Payload: %s\n", json_object_get_string(Size_UDP_Payload));
+  printf("Inter_Measurement_Time: %s\n", json_object_get_string(Inter_Measurement_Time));
+  printf("Number_UDP_Packets: %d\n", json_object_get_int(Number_UDP_Packets));
+  printf("TTL_UDP_Packets: %d\n", json_object_get_int(TTL_UDP_Packets));
      return 0; 
 }
