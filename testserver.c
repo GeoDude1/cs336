767facd1295689449118fa7a22a2914516720f4a
Gerardo Aldana <47541281+GeoDude1@users.noreply.github.com>
2020-04-14 10:19:01 -0700

    Update testserver.c

--- testserver.c
+++ testserver.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <ctype.h>
+#include <json-c/json.h>
 
 
 void error(const char *msg)
@@ -18,7 +19,8 @@ int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno;
      socklen_t clilen;
-     char buffer[512];
+     FILE *fp;
+     char buffer[1024];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
      if (argc < 2) {
@@ -48,19 +50,48 @@ int main(int argc, char *argv[])
           
           FILE *fp;
          int ch = 0;
-            fp = fopen("glad_receive.txt","a");            
+            fp = fopen("myconfig.json","a");            
             int words;
-		read(newsockfd, &words, sizeof(int));
+      read(newsockfd, &words, sizeof(int));
             //printf("Passed integer is : %d\n" , words);      //Ignore , Line for Testing
           while(ch != words)
-       	   {
-        	 read(newsockfd , buffer , 512); 
-	   	 fprintf(fp , " %s" , buffer);   
-		 //printf(" %s %d "  , buffer , ch); //Line for Testing , Ignore
-		 ch++;
-	   }
-     	printf("The file was received successfully\n");
-	   printf("The new file created is glad5.txt");
+            {
+          read(newsockfd , buffer , 512); 
+          fprintf(fp , " %s" , buffer);   
+       //printf(" %s %d "  , buffer , ch); //Line for Testing , Ignore
+       ch++;
+      }
+      printf("The file was received successfully\n");
+      printf("The new file created is glad5.txt");
+
+      struct json_object *parsed_json; //structure that holds parsed JSON
+   //stores rest of fields of the JSON file
+   struct json_object *Server_IP_Address;
+   struct json_object *Source_Port_Number_UDP;
+   struct json_object *Destination_Port_Number_TCP_Head;
+   struct json_object *Destination_Port_Number_TCP_Tail;
+   struct json_object *Port_Number_TCP;
+   struct json_object *Size_UDP_Payload;
+   struct json_object *Inter_Measurement_Time;
+   struct json_object *Number_UDP_Packets;
+   struct json_object *TTL_UDP_Packets;
+
+   fp = fopen("myconfig.json","r"); //opens file
+   fread(buffer, 1024, 1, fp); //reads files and puts contents inside buffer
+   fclose(fp);
+
+   parsed_json = json_tokener_parse(buffer); //parse json file's contents and convert them into a json object
+
+   json_object_object_get_ex(parsed_json, "Server_IP_Address", &Server_IP_Address);
+   json_object_object_get_ex(parsed_json, "Source_Port_Number_UDP", &Source_Port_Number_UDP);
+   json_object_object_get_ex(parsed_json, "Destination_Port_Number_TCP_Head", &Destination_Port_Number_TCP_Head);
+   json_object_object_get_ex(parsed_json, "Destination_Port_Number_TCP_Tail", &Destination_Port_Number_TCP_Tail);
+   json_object_object_get_ex(parsed_json, "Port_Number_TCP", &Port_Number_TCP);
+   json_object_object_get_ex(parsed_json, "Size_UDP_Payload", &Size_UDP_Payload);
+   json_object_object_get_ex(parsed_json, "Inter_Measurement_Time", &Inter_Measurement_Time);
+   json_object_object_get_ex(parsed_json, "Number_UDP_Packets", &Number_UDP_Packets);
+   json_object_object_get_ex(parsed_json, "TTL_UDP_Packets", &TTL_UDP_Packets);
+
      close(newsockfd);
      close(sockfd);
      return 0; 
