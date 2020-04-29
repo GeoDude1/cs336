7e225fa84934b69fbd2e019bdfef0eb4b447f2f2
Gerardo Aldana <47541281+GeoDude1@users.noreply.github.com>
2020-04-28 17:27:48 -0700

    Add files via upload

--- standalone.c
+++ standalone.c
@@ -17,140 +17,14 @@
 #include <sys/ioctl.h> 
 #include <netinet/ip.h>
 #include <net/if.h>
-#include <sys/wait.h>
 
 #define BUF_SIZE 2000
 
 enum{UDP_HDRLEN=8, ICMP_HDRLEN=8, TCP_HDRLEN=20, IP4_HDRLEN=20};
 
-char * 
-allocate_strmem(int len)
-{
-    void *tmp;
 
-    if (len <= 0) {
-        fprintf(stderr, "ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
-        exit(EXIT_FAILURE);
-    }
-
-    tmp = (char *)malloc(len * sizeof (char));
-    if (tmp != NULL){
-        memset(tmp, 0, len * sizeof (char));
-        return(tmp);
-    } else {
-        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_strmem().\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-int * 
-allocate_intmem(int len)
-{
-    void *tmp;
-
-    if (len <= 0) {
-        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_intmem().\n",len);
-        exit(EXIT_FAILURE);
-    }
-
-    tmp = (int *)malloc(len * sizeof(int));
-    if(tmp != NULL){
-        memset (tmp, 0, len * sizeof(int));
-        return (tmp);
-    } else{
-        fprintf(stderr,"ERROR: Cannot allocate memory for array allocate_intmem().\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-uint8_t *
-allocate_ustrmem(int len)
-{
-    void *tmp;
-
-    if (len <= 0) {
-        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_ustrmem().\n", len);
-        exit (EXIT_FAILURE);
-    }
-
-    tmp = (uint8_t *) malloc (len * sizeof (uint8_t));
-    if (tmp != NULL) {
-        memset (tmp, 0, len * sizeof (uint8_t));
-        return (tmp);
-    } else {
-        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_ustrmem().\n");
-        exit (EXIT_FAILURE);
-    }
-}
-
-/**
- * @file packet_setup.c
- *
- * source code exploited by our clients and server for setting up packet ids,
-   reading packet payload, and initializing header and checksum
- *
- */
-
-
-/**
- * id packet attachment function
- *
- * this function is responsible for generating a unique id for each packet
- *
- * @param bin | this is the binary version of the packet id (a binary strings)
- *
- * @param val | this is the unique id
- *
- * Does not envoke any local helper functions
- */
-void 
-packet_id_setup (char* bin, unsigned int val)
-{
-    unsigned int copy_of_value = val;
-    for(int i = 15; i >= 0 ;i--)
-    {
-        bin[i] = (copy_of_value & 0b1) +'0';
-        copy_of_value >>= 1;
-    }
-}
-
-/**
- * high entropy payload setup function
- *
- * this function is responsible for generating and assigning data for a group of packets
- *
- * @param data | this is the group of packets we are sending out
- *
- * @param len | the amount of packets to fill in 
- *
- * Does not envoke any local helper functions
- */
-void 
-read_high_entropy_data(uint8_t * data, int len) 
-{
-    FILE* file_ptr = NULL;
-    file_ptr =  fopen("/dev/random", "r");
-    for (int i = 0; i < len-1; i++) {
-        data[i] = getc(file_ptr);
-    }
-    fclose(file_ptr);
-}
-
-
-/**
- * IP checksum setup function
- *
- * this function is responsible for creating a unique checksum for our IP headers
- *
- * @param addr | this is the address of the IP header
- *
- * @param len | the length of the checksums
- *
- * Does not envoke any local helper functions
- */
-uint16_t 
-checksum(uint16_t *addr, int len)
+//this function creates unique checksum for the IP headers
+uint16_t checksum(uint16_t *addr, int len)
 {
     int count = len;
     register uint32_t sum = 0;
@@ -180,22 +54,78 @@ checksum(uint16_t *addr, int len)
     return (answer);
 }
 
+//this function creates unique checksum for the UDP headers
+uint16_t udp4_checksum(struct ip iphdr, struct udphdr udphdr, uint8_t *payload, int payloadlen)
+{
+    char buf[IP_MAXPACKET];
+    char *ptr;
+    int chksumlen = 0;
+    int i;
 
-/**
- * TCP checksum setup function
- *
- * this function is responsible for creating a unique checksum for our TCP headers
- *
- * @param iphdr | this contains the IP header information
- *
- * @param tcphdr | this contains the TCP header information
- *
- * this function envokes checksum 
- * @see #checksum(uint16_t *addr, int len)
- *
- */
-uint16_t 
-tcp4_checksum (struct ip iphdr, struct tcphdr tcphdr)
+    ptr = &buf[0];  // ptr points to beginning of buffer buf
+
+    // Copy source IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
+    ptr += sizeof (iphdr.ip_src.s_addr);
+    chksumlen += sizeof (iphdr.ip_src.s_addr);
+
+    // Copy destination IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
+    ptr += sizeof (iphdr.ip_dst.s_addr);
+    chksumlen += sizeof (iphdr.ip_dst.s_addr);
+
+    // Copy zero field to buf (8 bits)
+    *ptr = 0; ptr++;
+    chksumlen += 1;
+
+    // Copy transport layer protocol to buf (8 bits)
+    memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
+    ptr += sizeof (iphdr.ip_p);
+    chksumlen += sizeof (iphdr.ip_p);
+
+    // Copy UDP length to buf (16 bits)
+    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+    ptr += sizeof (udphdr.len);
+    chksumlen += sizeof (udphdr.len);
+
+    // Copy UDP source port to buf (16 bits)
+    memcpy (ptr, &udphdr.source, sizeof (udphdr.source));
+    ptr += sizeof (udphdr.source);
+    chksumlen += sizeof (udphdr.source);
+
+    // Copy UDP destination port to buf (16 bits)
+    memcpy (ptr, &udphdr.dest, sizeof (udphdr.dest));
+    ptr += sizeof (udphdr.dest);
+    chksumlen += sizeof (udphdr.dest);
+
+    // Copy UDP length again to buf (16 bits)
+    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+    ptr += sizeof (udphdr.len);
+    chksumlen += sizeof (udphdr.len);
+
+    // Copy UDP checksum to buf (16 bits)
+    // Zero, since we don't know it yet
+    *ptr = 0; ptr++;
+    *ptr = 0; ptr++;
+    chksumlen += 2;
+
+    // Copy payload to buf
+    memcpy (ptr, payload, payloadlen);
+    ptr += payloadlen;
+    chksumlen += payloadlen;
+
+    // Pad to the next 16-bit boundary
+    for (i=0; i<payloadlen%2; i++, ptr++) {
+        *ptr = 0;
+        ptr++;
+        chksumlen++;
+    }
+
+    return checksum((uint16_t *)buf,chksumlen);
+}
+
+//this function creates unique checksum for the TCP headers
+uint16_t tcp4_checksum (struct ip iphdr, struct tcphdr tcphdr)
 {
     uint16_t svalue;
     char buf[IP_MAXPACKET], cvalue;
@@ -281,102 +211,78 @@ tcp4_checksum (struct ip iphdr, struct tcphdr tcphdr)
     return checksum ((uint16_t *) buf, chksumlen);
 }
 
-void 
-sigalarm_handler(int signo)
+// Allocate memory for an array of chars
+char * allocate_strmem(int len)
 {
-    printf("Program ran too long. Please wait a bit before rerunning. Shutting down...\n");
-    exit(EXIT_FAILURE);
-}
+    void *tmp;
 
-/**
- * UDP checksum setup function
- *
- * this function is responsible for creating a unique checksum for our UDP headers
- *
- * @param iphdr | this contains the IP header information
- *
- * @param udphdr | this contains the UDP header information
- *
- * @param payload | this is the body of the packet
- *
- * @param payloadlen | this is the length of the packet payload
- *
- * this function envokes checksum function
- * @see #checksum(uint16_t *addr, int len)
- *
- */
-uint16_t 
-udp4_checksum(struct ip iphdr, struct udphdr udphdr, uint8_t *payload, int payloadlen)
-{
-    char buf[IP_MAXPACKET];
-    char *ptr;
-    int chksumlen = 0;
-    int i;
-
-    ptr = &buf[0];  // ptr points to beginning of buffer buf
-
-    // Copy source IP address into buf (32 bits)
-    memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
-    ptr += sizeof (iphdr.ip_src.s_addr);
-    chksumlen += sizeof (iphdr.ip_src.s_addr);
-
-    // Copy destination IP address into buf (32 bits)
-    memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
-    ptr += sizeof (iphdr.ip_dst.s_addr);
-    chksumlen += sizeof (iphdr.ip_dst.s_addr);
-
-    // Copy zero field to buf (8 bits)
-    *ptr = 0; ptr++;
-    chksumlen += 1;
-
-    // Copy transport layer protocol to buf (8 bits)
-    memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
-    ptr += sizeof (iphdr.ip_p);
-    chksumlen += sizeof (iphdr.ip_p);
-
-    // Copy UDP length to buf (16 bits)
-    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
-    ptr += sizeof (udphdr.len);
-    chksumlen += sizeof (udphdr.len);
-
-    // Copy UDP source port to buf (16 bits)
-    memcpy (ptr, &udphdr.source, sizeof (udphdr.source));
-    ptr += sizeof (udphdr.source);
-    chksumlen += sizeof (udphdr.source);
-
-    // Copy UDP destination port to buf (16 bits)
-    memcpy (ptr, &udphdr.dest, sizeof (udphdr.dest));
-    ptr += sizeof (udphdr.dest);
-    chksumlen += sizeof (udphdr.dest);
-
-    // Copy UDP length again to buf (16 bits)
-    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
-    ptr += sizeof (udphdr.len);
-    chksumlen += sizeof (udphdr.len);
-
-    // Copy UDP checksum to buf (16 bits)
-    // Zero, since we don't know it yet
-    *ptr = 0; ptr++;
-    *ptr = 0; ptr++;
-    chksumlen += 2;
-
-    // Copy payload to buf
-    memcpy (ptr, payload, payloadlen);
-    ptr += payloadlen;
-    chksumlen += payloadlen;
-
-    // Pad to the next 16-bit boundary
-    for (i=0; i<payloadlen%2; i++, ptr++) {
-        *ptr = 0;
-        ptr++;
-        chksumlen++;
+    if (len <= 0) {
+        fprintf(stderr, "ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
+        exit(EXIT_FAILURE);
     }
 
-    return checksum((uint16_t *)buf,chksumlen);
+    tmp = (char *)malloc(len * sizeof (char));
+    if (tmp != NULL){
+        memset(tmp, 0, len * sizeof (char));
+        return(tmp);
+    } else {
+        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_strmem().\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
-int 
-main(int argc, char **argv)
+// Allocate memory for an array of unsigned chars.
+uint8_t * allocate_ustrmem(int len)
+{
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_ustrmem().\n", len);
+        exit (EXIT_FAILURE);
+    }
+
+    tmp = (uint8_t *) malloc (len * sizeof (uint8_t));
+    if (tmp != NULL) {
+        memset (tmp, 0, len * sizeof (uint8_t));
+        return (tmp);
+    } else {
+        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_ustrmem().\n");
+        exit (EXIT_FAILURE);
+    }
+}
+
+// Allocate memory for an array of ints.
+int * allocate_intmem(int len)
+{
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_intmem().\n",len);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp = (int *)malloc(len * sizeof(int));
+    if(tmp != NULL){
+        memset (tmp, 0, len * sizeof(int));
+        return (tmp);
+    } else{
+        fprintf(stderr,"ERROR: Cannot allocate memory for array allocate_intmem().\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//this function creates a unique id for each packet
+void packet_id_setup (char* bin, unsigned int val)
+{
+    unsigned int copy_of_value = val;
+    for(int i = 15; i >= 0 ;i--)
+    {
+        bin[i] = (copy_of_value & 0b1) +'0';
+        copy_of_value >>= 1;
+    }
+}
+
+int main(int argc, char **argv)
 {
     FILE * fp;
     char buffer[BUF_SIZE];
@@ -416,12 +322,6 @@ main(int argc, char **argv)
     json_object_object_get_ex(parsed_json, "TTL_UDP_Packets", &TTL_UDP_Packets);
     printf("Parsing Successful\n");
 
-    signal(SIGALRM, sigalarm_handler);
-    alarm(300);
-    /* 
-        * unlike our server_cooperative/client_cooperative  we are using raw sockets for deeper control over the
-            packet data specifications (layers and payload)
-    */
     int bytes;
 
     if (argc < 2 || argc > 3)
@@ -430,9 +330,8 @@ main(int argc, char **argv)
         exit (EXIT_FAILURE);
     }
     unsigned int packet_id = 0;
-    //read JSON file to obtain data (TTL will be our main variable)
-    
 
+    
     pid_t child = fork();
 
     if (child == -1)
@@ -752,9 +651,6 @@ main(int argc, char **argv)
         // UDP data
         memcpy (udp_pkt + IP4_HDRLEN + UDP_HDRLEN, data, datalen);
 
-        read_high_entropy_data(&data[16], json_object_get_int(Size_UDP_Payload)-16);
-
-
         // IPv4 header
         memcpy (udp_pkt_2, &iphdr, IP4_HDRLEN * sizeof (uint8_t));
 
@@ -789,7 +685,7 @@ main(int argc, char **argv)
 
         sleep(json_object_get_int(Inter_Measurement_Time));
 
-            // Send packet.
+        // Send packet.
         if (sendto (sd, tcp_pkt_hd, IP4_HDRLEN + TCP_HDRLEN, 0, (struct sockaddr *) &sin, sizeof (struct sockaddr)) < 0)  {
             perror ("sendto() failed ");
             exit (EXIT_FAILURE);
@@ -811,9 +707,6 @@ main(int argc, char **argv)
         }
 
         printf("Success: All required packets have been sent!\n");
-
-        int exit_status;
-        waitpid(child, &exit_status, 0);
 
         // Close socket descriptor.
         close (sd);
